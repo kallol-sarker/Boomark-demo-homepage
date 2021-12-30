39c806c2d8068730b47ee2061b99a44125899a74
kallol <kallolsarker72@gmail.com>
2021-12-30 18:34:58 +0600

    Bookmark Template Commited

---
+++ asset/js/main.js
@@ -0,0 +1,45 @@
+// $('.products').slick({
+//       infinite: true,
+//       slidesToShow: 6,
+//       slidesToScroll: 3,
+//       dots: true,
+//       responsive: [
+//         {
+//           breakpoint: 991,
+//           settings: {
+//             slidesToShow: 4,
+//             slidesToScroll: 3,
+//             infinite: true,
+//             dots: true
+//           }
+//         },
+//         {
+//           breakpoint: 768,
+//           settings: {
+//             slidesToShow: 3,
+//             slidesToScroll: 3,
+//             infinite: true,
+//             dots: true
+//           }
+//         },
+//         {
+//           breakpoint: 576,
+//           settings: {
+//             slidesToShow: 2,
+//             slidesToScroll: 3,
+//             infinite: true,
+//             dots: true
+//           }
+//         },
+//         {
+//           breakpoint: 479,
+//           settings: {
+//             slidesToShow: 1,
+//             slidesToScroll: 3,
+//             infinite: true,
+//             dots: true
+//           }
+//         },
+//       ]
+//     });
+  
